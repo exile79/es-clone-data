47b1dcca9971f3f0e24515dc66200302756c4dc0
george <george@lstechltd.co.uk>
2020-04-01 12:07:47 +0300

    config file and utf encoding

--- esfetch.py
+++ esfetch.py
@@ -1,28 +1,40 @@
+import configparser
 import requests
 import jmespath
 import json
 
-# TODO get query from configuration file
-URL = "http://localhost:9200/test1/_search"
-INDEX2WRITE = 'test3'
-TYPE2WRITE = '_doc'
+# get parameters from ini file
+config = configparser.ConfigParser()
+config.read('config.ini')
+USEFILE = config['DEFAULT']['UseFile']
+INPUTFILE = config['DEFAULT']['InputFile']
+OUTPUTFILE = config['DEFAULT']['OutputFile']
+URL = config['DEFAULT']['url']
+INDEX2WRITE = config['DEFAULT']['Index2Write']
+TYPE2WRITE = config['DEFAULT']['Type2Write']
+EXTRACTPATH = config['DEFAULT']['ExtractPath']
 
-# TODO try-catch exceptions
-r = requests.get(url=URL)
+rj = None
 
-rj = r.json()
+if USEFILE:
+    # get data from file
+    with open(INPUTFILE, 'r', encoding="utf8") as content_file:
+        content = content_file.read()
+        rj = json.loads(content)
+else:
+    # get data from a URL
+    r = requests.get(url=URL)
+    rj = content.json()
 
-# TODO parameterize the path we want to extract
-h = jmespath.search('hits.hits[*]._source', rj)
+if not rj:
+    exit
 
-file = open('myfile.json', 'w', newline='\n')
-
+h = jmespath.search(EXTRACTPATH, rj)
 bulk = '{"index":{"_index":"' + INDEX2WRITE + '","_type" :"' + TYPE2WRITE + '"}}'
 
-for s in h:
-    file.write(bulk+'\n')
-    file.write(json.dumps(s)+'\n')
-
-file.close()
+with open(OUTPUTFILE, 'w', newline='\n', encoding = "utf8") as outfile:
+    for s in h:
+        outfile.write(bulk+'\n')
+        outfile.write(json.dumps(s, ensure_ascii = False) + '\n')
 
 print('Finished. Written ' + str(len(h)) + ' entries')
