bc4d81d3ff66b041d7314fd488cf4c97103bbe54
george <george@lstechltd.co.uk>
2020-05-01 22:42:21 +0300

    changes

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # es-clone-data
-use an Elastic search query to get data from an index and prepare a json for bulk writing to another one
+esfetch.py uses ElasticSearch data (fetched from a url or from a file) to generate data ready to be uploaded using the [BULK API](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html)
+
+Configuration using the config.ini file
+---
+[DEFAULT]
+
+* USEFILE: True/False, if False script will use the URL instead of loacl file
+* USEPREFIX: True/False, if False, INPUTFILE is used as it is, otherwise all files that start with it will be used (ex. input_01.json, input_02.json, input_foo.json...)
+* INPUTFILE: full filename if USEPREFIX=False, prefix otherwise
+* EXTRACTPATH:  the json path to extract from the input files, (example hits.hits[*])
+* URL: The cluser url, including port, username and password. Used if USEFILE=False. Example https//user:pass@localhost:9200
+* INDICES: index patterns to fecth from Url, comma separated. Example index1*,logstash*,foo*. Script will generate one output file per index pattern.
+* MAXQUERY: Size of the query results
+* GENERATEUPLOADSCRIPT: True/False, to create or nor a bash script for uploading the data generated
+* INCLUDEINDICES: True/False If creation of the correspondinfg indices shoud be included in the load bash script
+* INCLUDEMAPPINGS: True/False if putting of mappings shoudl be included for ht ecreated inidces. Mappings are fetched from url in separate query
+* TARGETURL: The url of the target cluster to use in the bash load script, Example http://localhost:9200
 
 
-Query to import the resulting data:
+Query to import the resulting data
+---
+Example:
+```
 curl -X POST http://localhost:9200/_bulk  -H 'Content-Type: application/json' --data-binary "@output.json"
--- esfetch.py
+++ esfetch.py
@@ -1,56 +1,92 @@
 import configparser
-import requests
+from elasticsearch import Elasticsearch
 import jmespath
 import json
 from os import listdir
-from os.path import isfile, join,splitext
+from os.path import isfile, join, splitext
 
 # get parameters from ini file
 config = configparser.ConfigParser()
 config.read('config.ini')
-USEFILE = config['DEFAULT']['UseFile']
-USEPREFIX = config['DEFAULT']['UsePrefix']
-INPUTFILE = config['DEFAULT']['InputFile']
-OUTPUTFILE = config['DEFAULT']['OutputFile']
-URL = config['DEFAULT']['url']
-INDEX2WRITE = config['DEFAULT']['Index2Write']
-TYPE2WRITE = config['DEFAULT']['Type2Write']
-EXTRACTPATH = config['DEFAULT']['ExtractPath']
+cnf = config['DEFAULT']
 
-rj = []
+outfilenames = []
+mappings = []
+indices = set()
 
-if USEFILE:
-    fnames = []
-    if USEPREFIX:
-        fnames = [f for f in listdir('.') if f.startswith(INPUTFILE)]
-    else:
-        fnames = [INPUTFILE]
 
-    for f in fnames:
+def dump_content(filename, str_content):
+
+    # gets the json inside the EXTRACTPATH (most likely hits.hits[*])
+    hits = jmespath.search(cnf['EXTRACTPATH'], str_content)
+
+    # writes 2 lines, required by the BULK API
+    def dumpline(h):
+        indices.add(h['_index'])
+        return ('{"index":{"_index":"' + h['_index'] + '","_id" :"' + h['_id'] + '"}}\n' +
+                json.dumps(h['_source'], ensure_ascii=False) + '\n')
+
+    lines = [dumpline(h) for h in hits]
+
+    filename = 'out_' + filename + '.json'
+    outfilenames.append(filename)
+
+    with open(filename, 'w', newline='\n', encoding="utf8") as outfile:
+        outfile.writelines(lines)
+
+    print('written file ' + filename + ' (' + str(len(hits)) + ' entries)')
+
+
+if cnf.getboolean('USEFILE'):
+    # get the file (or list of files if using prefix)
+    filenames = [f for f in listdir('.') if f.startswith(
+        cnf['INPUTFILE'])] if cnf.getboolean('USEPREFIX') else [cnf['INPUTFILE']]
+
+    # rprocess and dump the files one-by-one
+    for f in filenames:
         with open(f, 'r', encoding="utf8") as content_file:
-            content = content_file.read()
-            rj.append(json.loads(content))
+            filename, ext = splitext(f)
+            dump_content(filename, json.loads(content_file.read()))
 else:
     # get data from a URL
-    r = requests.get(url=URL)
-    rj = [content.json()]
 
-if len(rj) < 1:
-    exit
+    # get the list of index patterns to search
+    patterns = list(map(str.strip, cnf['INDICES'].split(',')))
 
-hh = []
-for rr in rj:
-    hh.append(jmespath.search(EXTRACTPATH, rr))
+    # connect to elastic ignoring ssl check. user/pass, if any, should be inculded in the url
+    es = Elasticsearch([cnf['URL']], verify_certs=False)
 
-bulk = '{"index":{"_index":"' + INDEX2WRITE + '","_type" :"' + TYPE2WRITE + '"}}'
+    # search, process and dump index patterns one-by-one
+    for ind in patterns:
+        if cnf.getboolean('INCLUDEMAPPINGS'):
+            m = es.indices.get_mapping(index=ind)
+            ms = jmespath.search('*.mappings.properties', m)
+            mappings.append((ind, str(ms[0]).replace("'", '"')))
 
-cnt = 1
-for h in hh:
-    filename, file_extension = splitext(OUTPUTFILE)
-    of = filename + str(cnt) + file_extension
-    cnt = cnt + 1
-    with open(of, 'w', newline='\n', encoding = "utf8") as outfile:
-        for s in h:
-            outfile.write(bulk+'\n')
-            outfile.write(json.dumps(s, ensure_ascii = False) + '\n')
-    print('Finished. Written ' + str(len(h)) + ' entries')
+        resp = es.search(index=ind, size=cnf.getint('MAXQUERY'))
+        filename = ind.strip('*')
+        dump_content(filename, resp)
+
+if cnf.getboolean('GENERATEUPLOADSCRIPT'):
+    lines = ['#!/bin/bash']
+
+    if cnf.getboolean('INCLUDEINDICES'):
+        lines.append('\n\necho "puting indices..."')
+        s = "\ncurl -X PUT " + cnf['TARGETURL'] + "/"
+        lines = lines + list([s + i for i in indices])
+
+    if cnf.getboolean('INCLUDEMAPPINGS'):
+        lines.append('\n\necho "puting mappings..."')
+        s = "\ncurl -X PUT " + \
+            cnf['TARGETURL'] + \
+            "/{0}/_mapping -H 'Content-Type: application/json' -d'{{\"properties\":{1}}}'"
+        lines = lines + list([s.format(m[0], m[1]) for m in mappings])
+
+    lines.append('\n\necho "posting data..."')
+    s = "\ncurl -o /dev/null -X POST " + \
+        cnf['TARGETURL'] + "/_bulk  -H 'Content-Type: application/json' --data-binary '@"
+    lines = lines + list([s + f + "'" for f in outfilenames])
+
+    with open("setup.sh", 'w', newline='\n', encoding="utf8") as outfile:
+        for line in lines:
+            outfile.write(line)
